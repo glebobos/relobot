5cc5a25fa4efa245d691574c1f8f3b4cfeda9dba
Hleb <gle605@gmail.com>
2025-03-30 18:26:26 +0100

    FEAT: add laser filter

--- ros2_ws/src/arducam_rclpy_tof_pointcloud/launch/launch.py
+++ ros2_ws/src/arducam_rclpy_tof_pointcloud/launch/launch.py
@@ -1,29 +1,46 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    # Path to your filter chain configuration file
+    config_file = os.path.join(
+        get_package_share_directory('arducam_rclpy_tof_pointcloud'),
+        'config',
+        'scan_filter_chain.yaml'
+    )
+
     return LaunchDescription([
+        # TOF pointcloud node
         Node(
             package='arducam_rclpy_tof_pointcloud',
             executable='tof_pointcloud',
             name='tof_pointcloud',
             output='screen'
         ),
+        # Convert pointcloud to raw laserscan
         Node(
             package='pointcloud_to_laserscan',
             executable='pointcloud_to_laserscan_node',
             name='pointcloud_to_laserscan',
             output='screen',
-            parameters=[
-                {
-                    'min_height': -0.1,
-                    'max_height': 0.1,
-                    'angle_min': -0.60,
-                    'angle_max': 0.39,
-                    'range_max': 4.0,
-                    'angle_increment': 0.018,
-                }
-            ],
+            parameters=[{
+                'min_height': -0.1,
+                'max_height': 0.1,
+                'angle_min': -0.60,
+                'angle_max': 0.39,
+                'range_max': 4.0,
+                'angle_increment': 0.018,
+            }],
             remappings=[('/cloud_in', '/cloud_in')]
+        ),
+        # Apply laser scan filters using the filter chain from laser_filters
+        Node(
+            package='laser_filters',
+            executable='scan_to_scan_filter_chain',
+            name='scan_filter_chain',
+            output='screen',
+            parameters=[config_file],
         )
     ])
--- ros2_ws/src/arducam_rclpy_tof_pointcloud/setup.py
+++ ros2_ws/src/arducam_rclpy_tof_pointcloud/setup.py
@@ -6,12 +6,13 @@ package_name = 'arducam_rclpy_tof_pointcloud'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.2',  # incremented version for new features
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),  # include config files
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -26,7 +27,7 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    description='Arducam Tof Camera Examples.',
+    description='Arducam TOF Camera Examples with additional laser scan filtering and interpolation.',
     license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
